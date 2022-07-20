3c146a87198d937bb50f4d0b42d5297811bac9db
Kežić Nenad <kezicnenad@gmail.com>
2022-07-20 23:36:57 +0200

    Solving problems part 2

--- js/script.js
+++ js/script.js
@@ -48,28 +48,53 @@ let computerSelection = "";
 let playerResult = 0;
 let computerResult = 0;
 
-function computerChoice() {
+let roundCounter = 0;
+
+function computersChoice() {
   const randomizeChoice = Math.floor(Math.random() * 3);
 
   switch (randomizeChoice) {
     case 0:
-      console.log("Rock");
-      computerSelection = "Rock";
+      console.log("Computer: Rock");
+      computerSelection = "Computer: Rock";
       break;
     case 1:
-      console.log("Scissors");
-      computerSelection = "Scissors";
+      console.log("Computer: Scissors");
+      computerSelection = "Computer: Scissors";
       break;
     case 2: 
-      console.log("Paper");
-      computerSelection = "Paper";
+      console.log("Computer: Paper");
+      computerSelection = "Computer: Paper";
   }
 }
 
 function playGame() {
-  playerSelection = prompt("Enter Rock, Paper or Scissors");
-  console.log(playerSelection);
-  computerChoice();
+  playerSelection = prompt("Enter Rock, Paper or Scissors").toLowerCase();
+  console.log("Player:", playerSelection);
+  computersChoice();
+
+  checkDetails(playerSelection, computerSelection);
 }
 
+function checkDetails(player, computer) {
+  console.log("P", player, "C", computer);
+
+  if (player === "rock" || player === "paper" || player === "scissors") {
+    console.log("Correct entry");
+
+    roundCounter++;
+    
+    // UVJET AKO JE MANJE OD 5 KOLA
+    // UVJET KO JE POBJEDNIK
+
+    // UVJET AKO JE VIŠE OD 5 KOLA DAJ POBJEDNIKA
+    playGame();
+
+  } else {
+    console.log("Incorrect entry. Try again");
+    playGame();
+  }
+}
+
+console.log("Enter Rock, Paper or Scissors");
 playGame();
