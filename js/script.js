131efe432d412f8253872616f328a123b149b27f
Kežić Nenad <kezicnenad@gmail.com>
2022-07-20 23:42:05 +0200

    Solving problems part 2

--- js/script.js
+++ js/script.js
@@ -1,3 +1,60 @@
+let playerSelection = "";
+let computerSelection = "";
+
+let playerResult = 0;
+let computerResult = 0;
+
+let roundCounter = 0;
+
+function computersChoice() {
+  const randomizeChoice = Math.floor(Math.random() * 3);
+
+  switch (randomizeChoice) {
+    case 0:
+      console.log("Computer: rock");
+      computerSelection = "Computer: rock";
+      break;
+    case 1:
+      console.log("Computer: scissors");
+      computerSelection = "Computer: scissors";
+      break;
+    case 2:
+      console.log("Computer: paper");
+      computerSelection = "Computer: paper";
+  }
+}
+
+function playGame() {
+  console.log("Round", roundCounter + 1);
+
+  playerSelection = prompt("Enter Rock, Paper or Scissors").toLowerCase();
+  console.log("Player:", playerSelection);
+  computersChoice();
+
+  checkDetails(playerSelection, computerSelection);
+}
+
+function checkDetails(player, computer) {
+  if (player === "rock" || player === "paper" || player === "scissors") {
+    console.log("Correct entry");
+
+    roundCounter++;
+
+    // UVJET AKO JE MANJE OD 5 KOLA
+    // UVJET KO JE POBJEDNIK
+
+    // UVJET AKO JE VIŠE OD 5 KOLA DAJ POBJEDNIKA
+    playGame();
+  } else {
+    console.log("Incorrect entry. Try again");
+    playGame();
+  }
+}
+
+console.log("Enter Rock, Paper or Scissors");
+playGame();
+
+
 // let playerSelection = prompt("Enter");
 // let computerSelection;
 
@@ -41,60 +98,3 @@
 // for (let i = 0; i < 5; i++) {
 //   playGame(playerSelection, computerSelection);
 // }
-
-let playerSelection = "";
-let computerSelection = "";
-
-let playerResult = 0;
-let computerResult = 0;
-
-let roundCounter = 0;
-
-function computersChoice() {
-  const randomizeChoice = Math.floor(Math.random() * 3);
-
-  switch (randomizeChoice) {
-    case 0:
-      console.log("Computer: Rock");
-      computerSelection = "Computer: Rock";
-      break;
-    case 1:
-      console.log("Computer: Scissors");
-      computerSelection = "Computer: Scissors";
-      break;
-    case 2: 
-      console.log("Computer: Paper");
-      computerSelection = "Computer: Paper";
-  }
-}
-
-function playGame() {
-  playerSelection = prompt("Enter Rock, Paper or Scissors").toLowerCase();
-  console.log("Player:", playerSelection);
-  computersChoice();
-
-  checkDetails(playerSelection, computerSelection);
-}
-
-function checkDetails(player, computer) {
-  console.log("P", player, "C", computer);
-
-  if (player === "rock" || player === "paper" || player === "scissors") {
-    console.log("Correct entry");
-
-    roundCounter++;
-    
-    // UVJET AKO JE MANJE OD 5 KOLA
-    // UVJET KO JE POBJEDNIK
-
-    // UVJET AKO JE VIŠE OD 5 KOLA DAJ POBJEDNIKA
-    playGame();
-
-  } else {
-    console.log("Incorrect entry. Try again");
-    playGame();
-  }
-}
-
-console.log("Enter Rock, Paper or Scissors");
-playGame();
