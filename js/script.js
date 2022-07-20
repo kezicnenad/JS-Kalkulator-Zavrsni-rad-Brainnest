34d05e4445124b9dcf43d464937a38a82f99c013
Kežić Nenad <kezicnenad@gmail.com>
2022-07-20 21:54:05 +0200

    Solving problems part 1

--- js/script.js
+++ js/script.js
@@ -1,43 +1,75 @@
-let playerSelection = prompt("Enter");
-let computerSelection;
+// let playerSelection = prompt("Enter");
+// let computerSelection;
 
-function computerPlay() {
-  let random = Math.floor(Math.random() * 3);
-  switch (random) {
+// function computerPlay() {
+//   let random = Math.floor(Math.random() * 3);
+//   switch (random) {
+//     case 0:
+//       computerSelection = "Rock";
+//       break;
+//     case 1:
+//       computerSelection = "Scissors";
+//       break;
+//     case 2:
+//       computerSelection = "Paper";
+//   }
+// }
+
+// computerPlay();
+
+// function playGame(player, computer){
+//   const playerSelectionToLowercase = player.toLowerCase();
+//   const computerSelectionToLowercase = computer.toLowerCase();
+
+//   if (
+//     playerSelectionToLowercase === "rock" && computerSelectionToLowercase === "scissors"
+//   ) {
+//     alert("Player win");
+//   } else if (
+//     playerSelectionToLowercase === "scissors" && computerSelectionToLowercase === "paper"
+//   ) {
+//     alert("Player win");
+//   } else if (
+//     playerSelectionToLowercase === "paper" && computerSelectionToLowercase === "rock"
+//   ) {
+//     alert("Player win");
+//   } else {
+//     alert("Computer win");
+//   }
+// }
+
+// for (let i = 0; i < 5; i++) {
+//   playGame(playerSelection, computerSelection);
+// }
+
+let playerSelection = "";
+let computerSelection = "";
+
+let playerResult = 0;
+let computerResult = 0;
+
+function computerChoice() {
+  const randomizeChoice = Math.floor(Math.random() * 3);
+
+  switch (randomizeChoice) {
     case 0:
+      console.log("Rock");
       computerSelection = "Rock";
       break;
     case 1:
+      console.log("Scissors");
       computerSelection = "Scissors";
       break;
-    case 2:
+    case 2: 
+      console.log("Paper");
       computerSelection = "Paper";
   }
 }
 
-computerPlay();
-
-function playGame(player, computer){
-  const playerSelectionToLowercase = player.toLowerCase();
-  const computerSelectionToLowercase = computer.toLowerCase();
-
-  if (
-    playerSelectionToLowercase === "rock" && computerSelectionToLowercase === "scissors"
-  ) {
-    alert("Player win");
-  } else if (
-    playerSelectionToLowercase === "scissors" && computerSelectionToLowercase === "paper"
-  ) {
-    alert("Player win");
-  } else if (
-    playerSelectionToLowercase === "paper" && computerSelectionToLowercase === "rock"
-  ) {
-    alert("Player win");
-  } else {
-    alert("Computer win");
-  }
+function playGame() {
+  playerSelection = prompt("Enter Rock, Paper or Scissors");
+  console.log(playerSelection);
+  computerChoice();
 }
 
-for (let i = 0; i < 5; i++) {
-  playGame(playerSelection, computerSelection);
-}
+playGame();
