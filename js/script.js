9d3c846fcd26dd4dff15e7cbea12e17a3e7cbb7c
Kežić Nenad <kezicnenad@gmail.com>
2022-07-22 18:57:55 +0200

    JS Vježba

--- js/script.js
+++ js/script.js
@@ -1,80 +1,27 @@
-let playerSelection = "";
-let computerSelection = "";
+let display = document.getElementById("display");
 
-let playerResult = 0;
-let computerResult = 0;
+let buttons = Array.from(document.getElementsByClassName("button"));
 
-let roundCounter = 0;
-
-function computersChoice() {
-  const randomizeChoice = Math.floor(Math.random() * 3);
-
-  switch (randomizeChoice) {
-    case 0:
-      console.log("Computer: rock");
-      computerSelection = "Computer: rock";
-      break;
-    case 1:
-      console.log("Computer: scissors");
-      computerSelection = "Computer: scissors";
-      break;
-    case 2:
-      console.log("Computer: paper");
-      computerSelection = "Computer: paper";
-  }
-}
-
-function playGame() {
-  console.log("Round", roundCounter + 1);
-
-  playerSelection = prompt("Enter Rock, Paper or Scissors").toLowerCase();
-  console.log("Player:", playerSelection);
-  computersChoice();
-
-  checkDetails(playerSelection, computerSelection);
-
-  if (playerSelection === "rock" && computerSelection === "scissors") {
-    playerResult++;
-  } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    playerResult++;
-  } else if (playerSelection === "paper" && computerSelection === "rock") {
-    playerResult++;
-  } else if (playerSelection === computerSelection) {
-    playerResult = playerResult;
-    computerResult = computerResult;
-  } else {
-    computerResult++;
-  }
-
-  if (playerResult > computerResult) {
-    console.log("Player wins");
-  } else if (playerResult === computerResult) {
-    console.log("Tye");
-  } else {
-    console.log("Computer wins");
-  }
-}
-
-function checkDetails(player, computer) {
-  if (player === "rock" || player === "paper" || player === "scissors") {
-    console.log("Correct entry");
-
-    roundCounter++;
-
-    if (roundCounter < 5){
-      playGame();
-    } else {
-      console.log("Igra gotova");
+buttons.map((button) => {
+  button.addEventListener("click", (e) => {
+    switch (e.target.innerText) {
+      case "C":
+        display.innerText = "";
+        break;
+      case "=":
+        try {
+          display.innerText = eval(display.innerText);
+        } catch {
+          display.innerText = "Error";
+        }
+        break;
+      case "←":
+        if (display.innerText) {
+          display.innerText = display.innerText.slice(0, -1);
+        }
+        break;
+      default:
+        display.innerText += e.target.innerText;
     }
-    // UVJET AKO JE MANJE OD 5 KOLA
-    // UVJET KO JE POBJEDNIK
-
-    // UVJET AKO JE VIŠE OD 5 KOLA DAJ POBJEDNIKA
-  } else {
-    console.log("Incorrect entry. Try again");
-    playGame();
-  }
-}
-
-console.log("Enter Rock, Paper or Scissors");
-playGame();
+  });
+});
