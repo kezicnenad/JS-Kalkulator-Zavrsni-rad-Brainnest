c614566bc56ff075501715983a41c4f34980c897
Kežić Nenad <kezicnenad@gmail.com>
2022-07-23 01:56:39 +0200

    Calculator

--- js/script.js
+++ js/script.js
@@ -1,27 +1,17 @@
-let display = document.getElementById("display");
+function display(val) {
+  document.getElementById("result").value += val;
+  return val;
+}
 
-let buttons = Array.from(document.getElementsByClassName("button"));
+function solve() {
+  let x = document.getElementById("result").value;
 
-buttons.map((button) => {
-  button.addEventListener("click", (e) => {
-    switch (e.target.innerText) {
-      case "C":
-        display.innerText = "";
-        break;
-      case "=":
-        try {
-          display.innerText = eval(display.innerText);
-        } catch {
-          display.innerText = "Error";
-        }
-        break;
-      case "←":
-        if (display.innerText) {
-          display.innerText = display.innerText.slice(0, -1);
-        }
-        break;
-      default:
-        display.innerText += e.target.innerText;
-    }
-  });
-});
+  let y = eval(x);
+  document.getElementById("result").value = y;
+  
+  return y;
+}
+
+function clearScreen() {
+  document.getElementById("result").value = "";
+}
